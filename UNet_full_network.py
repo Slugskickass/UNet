ce3e9ce1d41a3a4b5988a51a4dcd25ef4a486bac
Jonathan-07 <57955969+Jonathan-07@users.noreply.github.com>
2020-08-21 01:06:39 +0100

    Update UNet_full_network.py

--- UNet_full_network.py
+++ UNet_full_network.py
@@ -34,41 +34,85 @@ class Maxpool(nn.Module):
         return self.maxpool(x)
 
 
-# Use DoubleConv and Maxpool classes to construct 'AlongDown' class
-class AlongDown(nn.Module):
+# Use DoubleConv and Maxpool classes to construct 'DownAlong' class
+class DownAlong(nn.Module):
 
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv_maxpool = nn.Sequential(
-            DoubleConv(in_channels, out_channels),
-            Maxpool()
+        self.maxpool_conv = nn.Sequential(
+            Maxpool(),
+            DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x):
-        return self.conv_maxpool(x)
+        return self.maxpool_conv(x)
+
+
+#Expansive path
+class UpAlong(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, a1, a2):
+        a1 = self.up(a1)
+
+        diffY = a2.size()[2] - a1.size()[2]
+        diffX = a2.size()[3] - a1.size()[3]
+
+        a2 = F.pad(a2, [-diffX // 2, diffX // 2 - diffX,
+                        -diffY // 2, diffY // 2 - diffY])
+
+        x = torch.cat([a2, a1], dim=1)
+        return self.conv(x)
+
+
+# Final output conv
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
 
 
 # Create model
 class UNet(nn.Module):
-    def __init__(self, n_channels):
+    def __init__(self, n_channels, n_classes):
         super(UNet, self).__init__()
         self.n_channels = n_channels
+        self.n_classes = n_classes
 
-        self.down1 = AlongDown(n_channels, 64)
-        self.down2 = AlongDown(64, 128)
-        self.down3 = AlongDown(128, 256)
-        self.down4 = AlongDown(256, 512)
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = DownAlong(64, 128)
+        self.down2 = DownAlong(128, 256)
+        self.down3 = DownAlong(256, 512)
+        self.down4 = DownAlong(512, 1024)
+        self.up1 = UpAlong(1024, 512)
+        self.up2 = UpAlong(512, 256)
+        self.up3 = UpAlong(256, 128)
+        self.up4 = UpAlong(128, 64)
+        self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
-        x1 = self.down1(x)
-        x2 = self.down2(x1)
-        x3 = self.down3(x2)
-        x4 = self.down4(x3)
-        return x4
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.up1(x5, x4)
+        x7 = self.up2(x6, x3)
+        x8 = self.up3(x7, x2)
+        x9 = self.up4(x8, x1)
+        output = self.outc(x9)
+        return output
 
 
 # Test model shapes
 t = torch.ones([1, 1, 572, 572])
-network = UNet(n_channels=1)
+network = UNet(n_channels=1, n_classes=2)
 pred = network(t)
 print(pred.shape)
